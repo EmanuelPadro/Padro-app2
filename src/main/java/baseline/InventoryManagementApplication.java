ed067bc9673d8e3133d4230384b9ded3d7a6d790
EmanuelPadro <emanuelpadro@knights.ucf.edu>
2021-11-18 18:24:45 -0500

    Initial commit

---
+++ src/main/java/baseline/InventoryManagementApplication.java
@@ -0,0 +1,15 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 2 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+
+public class InventoryManagementApplication {
+
+    public static void main(String[] args) {
+        //launch(args);
+    }
+    
+}
